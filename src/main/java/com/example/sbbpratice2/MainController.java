8338d638332196dba49d9d660c14a213356fb2ed
chengsukim <kcs981123@naver.com>
2024-01-23 12:21:22 +0900

    https://github.com/chengsukim/sbbpratice2.git

---
+++ src/main/java/com/example/sbbpratice2/MainController.java
@@ -0,0 +1,16 @@
+package com.example.sbbpratice2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "sbb에 오신것을 환영합니다";
+    }
+
+}
---
+++ src/main/java/com/example/sbbpratice2/Sbbpratice2Application.java
@@ -0,0 +1,13 @@
+package com.example.sbbpratice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbbpratice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbbpratice2Application.class, args);
+	}
+
+}
